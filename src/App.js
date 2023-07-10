015dbf16e3d6ac47df282fb6ab70c4133da9ed02
Gamal Hanfey Khattab <120048751+Gamal-Hanfey-Khattab@users.noreply.github.com>
2023-07-10 19:50:24 +0200

    Filter Part

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 
-import './App.css';
-
+import Mentee from './Componets/Mentee';
+import Mentor from './Componets/Mentor';
+import Opportunities from './Componets/Opportunities';
+import Main from './Componets/main';
+import Selectlist from './Componets/selectList';
+import { Routes, Route } from "react-router-dom"
+import Requests from './Componets/Requests';
 function App() {
   return (
-    <div className="App">
-     main
-    </div>
+    <>
+   
+      <Routes>
+        <Route path='' element={<Selectlist ></Selectlist>}>
+          <Route path='/:0?' element={<Mentor/>}/>
+          <Route path='/1' element={<Mentee/>}/>
+          <Route path='/2' element={<Opportunities/>}/>
+          <Route path='/3' element={<Requests/>}/>
+        </Route>
+      </Routes>
+    </>
   );
 }
 
